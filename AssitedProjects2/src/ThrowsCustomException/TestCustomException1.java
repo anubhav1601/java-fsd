8558944bc184b86f49d1be06c68c46c163cb62f7
anubhav <anubhavkadam16@gmail.com>
2022-07-23 16:16:11 +0530

    changes

--- AssitedProjects2/src/ThrowsCustomException/TestCustomException1.java
+++ AssitedProjects2/src/ThrowsCustomException/TestCustomException1.java
@@ -1,48 +1,33 @@
 package ThrowsCustomException;
 
-class  InvalidAgeException1  extends Exception  
-{  
-    public  InvalidAgeException1 (String str)  
-    {  
-       
-        super(str);  
-    }  
-}  
-    
+class InvalidAgeException1 extends Exception {
+	public InvalidAgeException1(String str) {
 
-public class TestCustomException1  
-{  
-  
-   
-    static void validate (int age) throws InvalidAgeException1{    
-       if(age < 18){  
-  
+		super(str);
+	}
+}
 
-        throw new InvalidAgeException1("age is not valid to vote");    
-    }  
-       else {   
-        System.out.println("welcome to vote");   
-        }   
-     }    
-  
-    
-    public static void main(String args[])  
-    {  
-        try  
-        {  
-              
-            validate(13);  
-        }  
-        catch (InvalidAgeException1 ex)  
-        {  
-            System.out.println("Caught the exception");  
-    
-        
-            System.out.println("Exception occured: " + ex);  
-        }  
-  finally {
-        System.out.println("rest of the code...");    
-    }  
-} } 
+public class TestCustomException1 {
 
+	static void validate(int age) throws InvalidAgeException1 {
+		if (age < 18) {
 
+			throw new InvalidAgeException1("age is not valid to vote");
+		} else {
+			System.out.println("welcome to vote");
+		}
+	}
+
+	public static void main(String args[]) {
+		try {
+
+			validate(13);
+		} catch (InvalidAgeException1 ex) {
+			System.out.println("Caught the exception");
+
+			System.out.println("Exception occured: " + ex);
+		} finally {
+			System.out.println("rest of the code...");
+		}
+	}
+}
